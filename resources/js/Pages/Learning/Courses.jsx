18b5cd89041481d096a42a7046e12c95cf01e8fb
Vasiliy Sizov <prog8@it-delta.ru>
2021-12-20 16:10:10 +0300

    refactor Courses state

--- resources/js/Pages/Learning/Courses.jsx
+++ resources/js/Pages/Learning/Courses.jsx
@@ -1,43 +1,39 @@
 import React, { useState } from 'react';
-// import Layout from './Layout'
-import { InertiaLink } from '@inertiajs/inertia-react';
 import SectionTabs from '../Components/SectionTabs.jsx';
 import SearchPanel from '../Components/SearchPanel.jsx';
 import CoursesList from '../Components/CoursesList.jsx';
 import ProgramsList from '../Components/ProgramsList.jsx';
 
 export default function Courses({ courses }) {
-  const [state, setState] = useState({
-    searchString: '',
-    viewTabs: [
-      {
-        name: 'Курсы',
-        href: '#',
-        current: true,
-        searchPlaceholder: 'Поиск по курсам'
-      },
-      {
-        name: 'Программы обучения',
-        href: '#',
-        current: false,
-        searchPlaceholder: 'Поиск по программам'
-      }
-    ],
-    programs: [
-      { id: 1, name: 'Программа 1', courses: [1, 2] },
-      { id: 2, name: 'Программа 2', courses: [3] }
-    ]
-  });
+  const [searchString, setSearchString] = useState('');
+  const [tabs, setTabs] = useState([
+    {
+      name: 'Курсы',
+      href: '#',
+      current: true,
+      searchPlaceholder: 'Поиск по курсам'
+    },
+    {
+      name: 'Программы обучения',
+      href: '#',
+      current: false,
+      searchPlaceholder: 'Поиск по программам'
+    }
+  ]);
+  const [programs, setPrograms] = useState([
+    { id: 1, name: 'Программа 1', courses: [1, 2] },
+    { id: 2, name: 'Программа 2', courses: [3] }
+  ]);
 
-  const handleSearch = (value) => setState({ ...state, searchString: value });
+  const handleSearch = (value) => setSearchString(value);
   const handleTabChange = (name) => {
-    const tabs = state.viewTabs.map((tab) => {
+    const newTabs = tabs.map((tab) => {
       tab.name === name
         ? tab.current = true
         : tab.current = false;
       return tab;
     });
-    setState({ ...state, viewTabs: tabs });
+    setTabs(newTabs);
   };
 
   return (
@@ -48,19 +44,19 @@ export default function Courses({ courses }) {
             <h2 className="text-3xl font-extrabold tracking-tight sm:text-4xl">Учебный центр</h2>
           </header>
           <div className="px-2 pb-5 sm:pb-0 border-b border-gray-200 flex-1 flex flex-wrap sm:flex-nowrap items-center justify-around sm:justify-between">
-            <SectionTabs tabs={state.viewTabs} onChange={handleTabChange} />
+            <SectionTabs tabs={tabs} onChange={handleTabChange} />
             <SearchPanel
               onChange={handleSearch}
-              placeholder={state.viewTabs.find((tab) => tab.current).searchPlaceholder}
+              placeholder={tabs.find((tab) => tab.current).searchPlaceholder}
             />
           </div>
           {(() => {
-            switch (state.viewTabs.find((tab) => tab.current).name) {
+            switch (tabs.find((tab) => tab.current).name) {
               case 'Курсы':
-                return <CoursesList courses={ courses.filter((course) => course.name.toLowerCase().includes(state.searchString)) } />;
+                return <CoursesList courses={ courses.filter((course) => course.name.toLowerCase().includes(searchString)) } />;
               case 'Программы обучения':
                 return <ProgramsList
-                  programs={state.programs.filter((program) => program.name.toLowerCase().includes(state.searchString)) }
+                  programs={programs.filter((program) => program.name.toLowerCase().includes(searchString)) }
                   courses={courses}
                 />;
               default:
