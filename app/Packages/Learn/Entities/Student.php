c4ec1cf8394cf0168b20518aac9b253c488d8544
Maxim Gerzon <gerzon@it-delta.ru>
2021-12-25 19:08:57 +0300

    Student class

--- app/Packages/Learn/Entities/Student.php
+++ app/Packages/Learn/Entities/Student.php
@@ -2,7 +2,24 @@
 
 namespace App\Packages\Learn\Entities;
 
+use App\Packages\Shared\Infrastructure\DTO\UserDTO;
+
+/**
+ *
+ */
 class Student
 {
+    public $id;
 
+    public function __construct(UserDTO $user) {
+        $this->id = $user->id;
+    }
+
+    public function getEndedCourses() {
+
+    }
+
+    public function getWorkingCourses() {
+
+    }
 }
