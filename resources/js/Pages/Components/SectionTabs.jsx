537df9cee98353d878f0d75ca0671a0e3d2c8f22
Vasiliy Sizov <prog8@it-delta.ru>
2021-12-30 13:54:45 +0300

    visual changes

--- resources/js/Pages/Components/SectionTabs.jsx
+++ resources/js/Pages/Components/SectionTabs.jsx
@@ -21,7 +21,7 @@ export default function SectionTabs({ tabs, onChange }) {
         <select
           id="current-tab"
           name="current-tab"
-          className="block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
+          className="block w-full focus:ring-indigo-500 focus:border-indigo-500 rounded-md"
           defaultValue={tabs.find((tab) => tab.current).name}
         >
           {tabs.map((tab) => (
@@ -30,17 +30,15 @@ export default function SectionTabs({ tabs, onChange }) {
         </select>
       </div>
       <div className="hidden sm:block">
-        <nav className="-mb-px flex space-x-8">
+        <nav className="flex space-x-4">
           {tabs.map((tab) => (
             <InertiaLink
               key={tab.name}
               name={tab.name}
               href={tab.href}
               className={classNames(
-                tab.current
-                  ? 'border-indigo-500 text-indigo-600'
-                  : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300',
-                'whitespace-nowrap pb-4 px-1 border-b-2 font-medium text-sm'
+                tab.current ? 'bg-indigo-100 text-indigo-700' : 'text-gray-500 hover:text-gray-700',
+                'px-3 py-2 font-medium text-sm rounded-md'
               )}
               aria-current={tab.current ? 'page' : undefined}
             >
