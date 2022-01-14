7a185ef920f786f143670e8cbb5b847da1cca36b
Vasiliy Sizov <prog8@it-delta.ru>
2022-01-14 14:28:25 +0300

    add link to main on logo in admin panel

--- resources/js/Admin/Navigation.jsx
+++ resources/js/Admin/Navigation.jsx
@@ -72,11 +72,13 @@ export default function Navigation({ navigation, children }) {
                 </div>
               </Transition.Child>
               <div className="flex-shrink-0 flex items-center px-4">
+                <InertiaLink href="/">
                 <img
                   className="h-8 w-auto"
                   src="https://tailwindui.com/img/logos/workflow-logo-indigo-300-mark-white-text.svg"
                   alt="Workflow"
-                />
+                  />
+                  </InertiaLink>
               </div>
               <div className="mt-5 flex-1 h-0 overflow-y-auto">
                 <nav className="px-2 space-y-1">
@@ -109,11 +111,13 @@ export default function Navigation({ navigation, children }) {
           {/* Sidebar component, swap this element with another sidebar if you like */}
           <div className="flex flex-col flex-grow pt-5 pb-4 overflow-y-auto">
             <div className="flex items-center flex-shrink-0 px-4">
-              <img
+              <InertiaLink href="/">
+                <img
                 className="h-8 w-auto"
                 src="https://tailwindui.com/img/logos/workflow-logo-indigo-300-mark-white-text.svg"
                 alt="Workflow"
-              />
+                />
+              </InertiaLink>
             </div>
             <div className="mt-5 flex-1 flex flex-col">
               <nav className="flex-1 px-2 space-y-1">
