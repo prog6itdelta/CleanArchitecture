cdd14d3a2e87f2b3ae5209c03e23c0d1d6677d90
Maxim Gerzon <gerzon@it-delta.ru>
2022-01-20 21:43:49 +0300

    deleted Clear button

--- resources/js/Pages/Profile.jsx
+++ resources/js/Pages/Profile.jsx
@@ -51,7 +51,8 @@ export default function Profile() {
   const onClear = () => {
     setAvatarFormImg(user.avatar);
     setNewPassword('');
-    reset();
+    setData('password', '');
+    // reset();
   };
 
   const onSubmit = (e) => {
@@ -235,15 +236,17 @@ export default function Profile() {
 
               <div className="pt-5">
                 <div className="flex justify-center py-1">
+                  {
+                  //   <button
+                  //   type="button"
+                  //   className="bg-white py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                  //   onClick={onClear}
+                  // >
+                  //   Clear
+                  // </button>
+                  }
                   <button
-                    type="button"
-                    className="bg-white py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                    onClick={onClear}
-                  >
-                    Clear
-                  </button>
-                  <button
-                    type="submit"
+                    type='submit'
                     className={classNames(
                       passwordsMatch ? '' : 'opacity-50 cursor-not-allowed',
                       'ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
