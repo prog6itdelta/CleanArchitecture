e89dc960bcecdb877ddf70fceb45613cc8f6343f
Maxim Gerzone <gerzon@it-delta.ru>
2022-03-05 15:14:19 +0300

    fix(couse): header and course container

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
+  mode: 'jit',
+
   purge: [
     './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
     './storage/framework/views/*.php',
     './resources/views/**/*.blade.php',
-    './resources/**/*.js',
+    './resources/**/*.{js,jsx}',
     './resources/**/*.svelte',
   ],
 
