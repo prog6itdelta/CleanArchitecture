a31ecfa81b767b0c33dfb0cb7277a104841266b7
Egorov Eduard <prog2@it-delta.ru>
2022-03-11 11:51:41 +0300

    fix:(StatusCell): tailwind text-color

--- resources/js/Components/StatusCell.jsx
+++ resources/js/Components/StatusCell.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const StatusCell = ({ value }) => {
   return (
-    <span className={`px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-${Number(value) === 1 ? 'green' : 'red'}-100 text-${Number(value) === 1 ? 'green' : 'red'}-800`}>
+    <span className={`px-2 inline-flex text-xs leading-5 font-semibold rounded-full ${Number(value) === 1 ? 'bg-green-100' : 'bg-red-100'} ${Number(value) === 1 ? 'text-green-800' : 'text-red-800'}`}>
       {Number(value) === 1 ? 'Active' : 'Inactive'}
     </span>
   );
