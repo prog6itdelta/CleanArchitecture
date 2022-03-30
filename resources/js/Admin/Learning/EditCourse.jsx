00e981b1204545554071a4aaca37e7e43fe5a674
Vasiliy Sizov <prog8@it-delta.ru>
2022-03-30 14:48:20 +0300

    fix non-editable course description

--- resources/js/Admin/Learning/EditCourse.jsx
+++ resources/js/Admin/Learning/EditCourse.jsx
@@ -108,6 +108,7 @@ export default function EditCourse({ course }) {
               <textarea
                 className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2 border-gray-300 rounded-md"
                 defaultValue={data.description}
+                onChange={(e) => setData('description', e.target.value)}
               />
             </li>
             <li className="bg-white px-4 py-5 sm:grid sm:grid-cols-2 sm:gap-4 sm:px-6 ">
